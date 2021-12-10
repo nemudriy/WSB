73b876bd4e7f0b58e05d8f2c09b3f95193fff26b
vyach <nemudriy19@gmail.com>
2021-12-10 23:57:31 +0300

    Current data meh

--- app/src/main/java/com/vyach/worldskillsbank/MainActivity.java
+++ app/src/main/java/com/vyach/worldskillsbank/MainActivity.java
@@ -8,15 +8,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+public class MainActivity extends AppCompatActivity {
+TextView currentData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button signin = findViewById(R.id.signInBtn);
+        currentData = findViewById(R.id.data_now);
+        getCurrentDate();
 
         signin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,8 +33,10 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
-
     }
-
+    public void getCurrentDate(){
+        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        Date date = new Date();
+        currentData.setText(dateFormat.format(date));
+    }
 }
